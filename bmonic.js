40b54fdda3014fd98bd6a5b1e2c7b78bda001b52
Hans Schoenburg <hschoenburg@gmail.com>
2018-03-27 10:44:51 -0700

    add passphrase minimum length

--- bmonic.js
+++ bmonic.js
@@ -9,6 +9,7 @@ const HDPrivateKey = require('./private');
 function bMonic (params) {
 
   this.newEncKey = (passphrase) => {
+    if(passphrase.length < 15) { throw new Error('passphrase must be at least 15 chars') }
     const pass = Buffer.from(passphrase)
 	  const mnemonic = new Mnemonic({language: params.lang})
     const phrase = mnemonic.getPhrase()
