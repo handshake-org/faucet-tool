326868dba78c8e4027ee8f4d5565286e87d78ca4
Hans Schoenburg <hschoenburg@gmail.com>
2018-04-16 21:11:59 -0700

    small fix

--- bmonic.js
+++ bmonic.js
@@ -24,7 +24,7 @@ function bMonic (params) {
     const addr = address.fromPubkey(Buffer.from(key.publicKey))
     return { 
       phrase: phrase,
-      address: addr.hash
+      address: addr.getHash('hex')
     }
   }
 }
