16f86ac1d48fb01fb921951f6678f92d9599f751
Boyma Fahnbulleh <boymanjor+github@protonmail.com>
2018-06-25 10:57:27 +0200

    cleanup bmonic.js

--- bmonic.js
+++ bmonic.js
@@ -7,13 +7,13 @@
  * but with Consensus and Network dependencies removed
  */
 
+
+const Address = require('./lib/address')
+const HDPrivateKey = require('./lib/private')
+const Mnemonic = require('./lib/mnemonic')
+
 const ENTROPY = 256
 const HNS_COIN_TYPE = 5353
-
-const Mnemonic = require('./lib/mnemonic')
-const HDPrivateKey = require('./lib/private')
-const Address = require('./lib/address')
-const {bech32} = require('bstring')
 
 function bMonic (params) {
 
@@ -22,7 +22,7 @@ function bMonic (params) {
     const master = HDPrivateKey.fromMnemonic(mnemonic)
 
     // Create a receive address from M/44'/5353'/0'/0/0.
-    // This is the first receiving address of the default (first) account.
+    // This is the first receiving address of the default/first account.
     const child = master.deriveAccount(44, HNS_COIN_TYPE, 0).derive(0).derive(0)
     const addr = Address.fromPubkey(child.publicKey)
 
@@ -31,14 +31,6 @@ function bMonic (params) {
       address: addr.toString('main')
     }
   }
-
-  this.testAddr = (addr) => {
-    return bech32.test(addr) // returns false
-  }
 }
 
-
 module.exports = bMonic
-//window.bMonic = bMonic
-
-
