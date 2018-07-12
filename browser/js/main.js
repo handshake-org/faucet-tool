d8972bab725cfb471b98fa0da58005e0f075a16d
Boyma Fahnbulleh <boymanjor@protonmail.com>
2018-07-12 15:05:19 -0700

    browser: include loading message before addrgen

--- browser/js/main.js
+++ browser/js/main.js
@@ -22,62 +22,81 @@
     }
 
     document.getElementById('participants').addEventListener('click', function(e) {
+      const address = tool.getAddress();
       const canvas = document.getElementById('canvas');
+      const loader = document.getElementById('loader');
       const hidden = document.getElementsByClassName('participants hide');
       const shown = document.getElementsByClassName('show button');
-      const address = tool.getAddress();
 
       e.preventDefault();
 
-      document.getElementById('phrase').innerHTML = tool.getPhrase();
-      document.getElementById('plaintext-participants').innerHTML = tool.getAddress();
-      document.getElementById('privkey').innerHTML = tool.getPrivkey();
-      document.getElementById('pubkey').innerHTML = tool.getPubkey();
-
-      while (hidden.length > 0) {
-        hidden[0].classList.remove('hide');
-      }
-
+      // Hide buttons
       for (let i = 0; i < shown.length; i++) {
         shown[i].classList.add('hide');
       }
+
+      // Display loader before displaying wallet details
+      loader.classList.remove('hide');
+
+      setTimeout(function() {
+        loader.classList.add('hide');
+
+        document.getElementById('phrase').innerHTML = tool.getPhrase();
+        document.getElementById('plaintext-participants').innerHTML = tool.getAddress();
+        document.getElementById('privkey').innerHTML = tool.getPrivkey();
+        document.getElementById('pubkey').innerHTML = tool.getPubkey();
+
+        while (hidden.length > 0) {
+          hidden[0].classList.remove('hide');
+        }
+      }, 1000)
     });
 
     document.getElementById('sponsors').addEventListener('click', function(e) {
       const address = tool.getAddress();
+      const shown = document.getElementsByClassName('show button');
 
       e.preventDefault();
 
-      FaucetTool.encryptData(address).then(function(ciphertext) {
-        const canvas = document.getElementById('canvas');
-        const hidden = document.getElementsByClassName('sponsors hide');
-        const shown = document.getElementsByClassName('show button');
+      for (let i = 0; i < shown.length; i++) {
+        shown[i].classList.add('hide');
+      }
 
-        QRCode.toCanvas(canvas, ciphertext, function(err, canvas) {
-          if (err) {
-            throw new Error(err);
-          }
+      // Display loader before displaying wallet details
+      loader.classList.remove('hide');
 
-          document.getElementById('ciphertext').innerHTML = ciphertext;
-          document.getElementById('phrase').innerHTML = tool.getPhrase();
-          document.getElementById('plaintext-sponsors').innerHTML = tool.getAddress();
-          document.getElementById('privkey').innerHTML = tool.getPrivkey();
-          document.getElementById('pubkey').innerHTML = tool.getPubkey();
+      setTimeout(function() {
+        FaucetTool.encryptData(address).then(function(ciphertext) {
+          const canvas = document.getElementById('canvas');
+          const loader = document.getElementById('loader');
+          const hidden = document.getElementsByClassName('sponsors hide');
 
-          while (hidden.length > 0) {
-            hidden[0].classList.remove('hide');
-          }
+          QRCode.toCanvas(canvas, ciphertext, function(err, canvas) {
+            if (err) {
+              throw new Error(err);
+            }
 
-          for (let i = 0; i < shown.length; i++) {
-            shown[i].classList.add('hide');
-          }
+            document.getElementById('ciphertext').innerHTML = ciphertext;
+            document.getElementById('phrase').innerHTML = tool.getPhrase();
+            document.getElementById('plaintext-sponsors').innerHTML = tool.getAddress();
+            document.getElementById('privkey').innerHTML = tool.getPrivkey();
+            document.getElementById('pubkey').innerHTML = tool.getPubkey();
+
+            loader.classList.add('hide');
+
+            while (hidden.length > 0) {
+              hidden[0].classList.remove('hide');
+            }
+          });
+        })
+        .catch(function(err) {
+          const errorBox = document.getElementById('error-box');
+
+          loader.classList.add('hide');
+          errorBox.classList.remove('hide');
+          errorBox.innerHTML = err;
         });
-      })
-      .catch(function(err) {
-        const errorBox = document.getElementById('error-box');
-        errorBox.classList.remove('hide');
-        errorBox.innerHTML = err;
-      });
+      }, 1000);
     });
   });
 }());
